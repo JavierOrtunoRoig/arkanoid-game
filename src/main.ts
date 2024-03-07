c2fe75135a58be0f45cae76b6d032e2286970fcb
Javier <javiortunoroig.com>
2024-03-07 17:13:54 +0100

    improve title screen

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import { $ } from './helpers';
 import './style.css';
 
 const startButton = $('#start-button') as HTMLButtonElement;
+
 startButton.addEventListener('click', () => {
   startButton.parentElement?.remove();
   const game = new Game();
   game.runGame();
 });
+
+function startGame(e: KeyboardEvent) {
+  if (e.key === 'Enter' || e.key === ' ') {
+    startButton.click();
+    document.removeEventListener('keydown', startGame);
+  }
+}
+document.addEventListener('keydown', startGame);
