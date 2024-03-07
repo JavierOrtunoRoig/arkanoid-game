bf1780622547d6afd7d91aa8387539c09881dbd0
Javier <javiortunoroig.com>
2024-03-07 19:06:30 +0100

    fix double game screen

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,21 @@ import { $ } from './helpers';
 import './style.css';
 
 const startButton = $('#start-button') as HTMLButtonElement;
+let isPlayerReady = false;
 
 startButton.addEventListener('click', () => {
   startButton.parentElement?.remove();
   const game = new Game();
   game.runGame();
+  isPlayerReady = true;
 });
 
 function startGame(e: KeyboardEvent) {
+  if (isPlayerReady) return;
   if (e.key === 'Enter' || e.key === ' ') {
     startButton.click();
     document.removeEventListener('keydown', startGame);
+    isPlayerReady = true;
   }
 }
 document.addEventListener('keydown', startGame);
